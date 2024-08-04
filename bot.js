2817ce6f9cd3756afcc43c5ad6f07b56d027afec
nubmaster <excapitalhold@icloud.com>
2024-08-03 17:00:34 -0700

    commit2

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 const port = process.env.PORT || 32020;
 
-const token = '7399253465:AAEZfwcFTCHU4_HPAgGWYTEUyKtaePboDTE';
+const token = '7376292606:AAHgBjsI_1HHq2NVJzhB5NfkK6-Ml8JS3Wo';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
@@ -19,7 +19,26 @@ bot.onText(/\/start/, (msg) => {
         [
           {
             text: 'SHOP',
-            web_app: { url: `https://cockies.onrender.com/?userId=${chatId}` }
+            web_app: { url: `https://trippy-66he.onrender.com/?userId=${chatId}` }
+          }
+        ]
+      ]
+    }
+  });
+  
+});
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+      console.log(chatId)
+// Corrected variable name
+  bot.sendMessage(chatId, 'SHOP', {
+
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'SHOP',
+            web_app: { url: `https://trippy-66he.onrender.com/admin/admin.html?userId=${chatId}` }
           }
         ]
       ]
